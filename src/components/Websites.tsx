5fdb116255510522b04b3c7f9f35ac4b501578de
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2023-02-19 08:33:54 -0300

    adicionando projeto

--- src/components/Websites.tsx
+++ src/components/Websites.tsx
@@ -11,10 +11,10 @@ const Websites = () => {
     <section style={{ backgroundImage: 'url(Untitled_design__8_-removebg-preview.png)' }}
       className="w-full h-full lg:h-[850px] bg-cover">
       <motion.div
-      variants={zoomIn(0.25,0.5)}
-      initial={"hidden"}
-      whileInView={"show"}
-      className='flex flex-col items-center mx-4 lg:mx-40 mt-12 text-justify justify-center gap-16 bg-gradient-to-tr from-cyan-500 via-purple-500 to-amber-500 p-4 rounded-lg drop-shadow-[1px_1px_5px_black]'>
+        variants={zoomIn(0.25, 0.5)}
+        initial={"hidden"}
+        whileInView={"show"}
+        className='flex flex-col items-center mx-4 lg:mx-40 mt-12 text-justify justify-center gap-16 bg-gradient-to-tr from-cyan-500 via-purple-500 to-amber-500 p-4 rounded-lg drop-shadow-[1px_1px_5px_black]'>
         <h2 className='text-2xl text-center md:text-4xl font-bold text-yellow-500 drop-shadow-[1px_1px_1px_black]'>
           Exemplos de sites feitos por nós
         </h2>
@@ -24,7 +24,7 @@ const Websites = () => {
             Os sites da KEEP são a escolha certa para quem busca uma presença online forte e eficaz. Nossos sites são responsivos, modernos e profissionais. Além disso, cada site é criado levando em consideração as necessidades e objetivos de cada cliente, garantindo assim uma experiência personalizada e única.
           </p>
 
-          <div className='w-[90%] xl:w-[1000px] xl:h-[500px] mx-auto mt-4 rounded-lg h-[300px] bg-yellow-500 flex flex-row gap-2 p-2'>
+          <div className='w-[90%] xl:w-[1000px] xl:h-[500px] mx-auto mt-4 rounded-lg h-[500px] bg-yellow-500 flex flex-col md:flex-row gap-2 p-2'>
 
             <div className={` flex-1 rounded-l-lg bg-center
              ${active == 0 && 'flex-[5] md:text-8xl text-5xl bg-cover'}
@@ -59,7 +59,16 @@ const Websites = () => {
               </a>
             </div>
 
+            <div className={` flex-1 rounded-l-lg bg-center
+             ${active == 3 && 'flex-[5] md:text-8xl text-5xl bg-cover'}
+              transition-all duration-1000 cursor-pointer bg-no-repeat  bg-white `} onClick={() => setActive(3)}
+              style={{ backgroundImage: 'url(rt.png)' }}>
 
+              <a className={`relative top-[50%] left-1/2 -translate-x-1/2 font-bold text-white drop-shadow-[1px_1px_1px_black] ${active !== 3 && 'hidden'}`}
+                href="http://miracleimoveis.com.br/">
+                <BsFillEyeFill />
+              </a>
+            </div>
 
           </div>
         </div>
