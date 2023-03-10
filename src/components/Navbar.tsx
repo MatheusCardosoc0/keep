e4a32c3ed14f7f2be82dfe6d4fad5518be4eb8cb
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2023-03-10 17:47:04 -0300

    teste

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,8 +12,7 @@ const Navbar = () => {
           className='md:w-[100px] w-[50px]'
         />
         <h1 className=' flex flex-col'>
-          <b className='drop-shadow-[2px_1px_0px_white] md:text-5xl font-bold bg-gradient-to-b from-cyan-400 via-purple-500 to-amber-500 bg-clip-text text-transparent'>KEEP</b>
-          <b className='md:text-xl text-sm text-cyan-400'>Maeketing e divulgações</b>
+          <b className='drop-shadow-[2px_1px_0px_white] md:text-5xl font-bold bg-gradient-to-b from-cyan-400 via-purple-500 to-amber-500 bg-clip-text text-transparent'>KEEPSOLUÇÕES</b>
         </h1>
       </span>
 
@@ -30,7 +29,7 @@ const Navbar = () => {
 
       {menuView && (
         <div className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col text-4xl gap-8 text-yellow-300 w-full h-screen text-center justify-center bg-purple-900/40'
-        onClick={() => setMenuView(false)}>
+          onClick={() => setMenuView(false)}>
           <a href='#about' className=' hover:text-cyan-400 drop-shadow-[1px_1px_2px_black] hover:underline cursor-pointer font-bold'>Sobre</a>
           <a href='#projects' className=' hover:text-cyan-400 drop-shadow-[1px_1px_2px_black] hover:underline cursor-pointer font-bold'>Projetos</a>
           <a href='#contact' className=' hover:text-cyan-400 drop-shadow-[1px_1px_2px_black] hover:underline cursor-pointer font-bold'>Contato</a>
